d7d1c93bbcdb83cd8f1646e3a678b9301b4b0c76
noisyZ <nero232@ukr.net>
2016-08-25 23:36:02 +0300

    Data Binding Library  v 0.1.3 Commit #4 Renamed modules

--- NoisyBindLibrary/src/main/java/com/noisyz/databindinglibrary/annotations/propertyType.java
+++
@@ -1,8 +0,0 @@
-package com.noisyz.databindinglibrary.annotations;
-
-/**
- * Created by Oleg on 18.03.2016.
- */
-public enum propertyType {
-    TEXT, BOOLEAN, FLOAT_TEXT, PROGRESS, RATING, RATING_CHANGEBLE, PROGRESS_CHANGEBLE, VISIBILITY, ENABLED
-}
--- NoisyBindLibrary/src/main/java/com/noisyz/databindinglibrary/beans/BindObject.java
+++
@@ -1,7 +0,0 @@
-package com.noisyz.databindinglibrary.beans;
-
-/**
- * Created by oleg on 13.08.16.
- */
-public class BindObject {
-}
